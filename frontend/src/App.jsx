d1704604fca7973d3acda456b2c3d60535d897a3
ikrameikram <ikrame-meilin@outlook.com>
2026-01-30 23:51:20 +0100

    backend complet

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-white">
+      <h1 className="text-xl font-sans">Bienvenue </h1>
+    </div>
+  )
+}
